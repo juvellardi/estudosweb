341ef99c1f35365f9185d84de68640b3a18f9bf2
juvellardi <julianavellardi@gmail.com>
2019-09-24 21:55:23 -0300

    tres projetos com html, css e js

---
+++ projeto3/assets/js/script.js
@@ -0,0 +1,16 @@
+// // alert("hello");
+var slideAtivo = document.querySelector('.slide.ativo');
+
+var proximo = slideAtivo.nextElementSibling;
+
+setInterval(function(){
+	var slideAtivo = document.querySelector('.slide.ativo');
+	var proximo = slideAtivo.nextElementSibling;
+
+	if(proximo == null){
+		proximo = document.querySelector('.slide');
+}
+		slideAtivo.classList.remove('ativo');
+		proximo.classList.add('ativo');
+
+	}, 3000);
